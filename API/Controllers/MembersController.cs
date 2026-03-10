36789b049f67c635940050c77c163817eb41dfdd
rohini-gaikawad <rohini.gaikawad@rib-software.com>
2026-03-10 16:28:47 +0530

    End of section 4

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,14 +1,13 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")] //localhost:5001/api/members(controllername) and in this case controller is MembersController so the name is for api/controler is api/Members so  the complete path is 
-    [ApiController]
-    public class MembersController(AppDbContext dbContext) : ControllerBase
+  //localhost:5001/api/members(controllername) and in this case controller is MembersController so the name is for api/controler is api/Members so  the complete path is (removed Api controller but only keep comment now)
+    public class MembersController(AppDbContext dbContext) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -18,6 +17,7 @@ namespace API.Controllers
             return members;
         }
 
+        [Authorize]
         [HttpGet("{id}")]  //localhost:5001/api/members/bob-id
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
